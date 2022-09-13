c26e0226f90f9515283b1e59e091def99bb16f5f
Rabbiya <malixrabbiya000@gmail.com>
2022-09-13 04:19:34 -0700

    modal added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import "./home.css";
 const Home = () => {
+  const [show, setShow] = useState(false);
+  const [modalData, setmodalData] = useState();
   const [count, setCount] = useState(0);
   const [data, setdata] = useState([]);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleShow = (item) => {
+    setmodalData(item);
+    setShow(true);
+  };
 
   let handlingIncrement = () => {
     setCount(count + 1);
@@ -43,8 +57,55 @@ const Home = () => {
         })}
       </ul> */}
 
+      <Modal
+        show={show}
+        onHide={() => {
+          handleClose();
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {" "}
+            <img src={modalData?.avatar} alt="he" />
+          </Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          {modalData?.email}
+          <br />
+          {modalData?.first_name}
+          <br />
+          {modalData?.last_name}
+          <br />
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              handleClose();
+            }}
+          >
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            onClick={() => {
+              handleClose();
+            }}
+          >
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       {data?.map((item, index) => (
-        <div className="mainDiv">
+        <div
+          className="mainDiv"
+          onClick={() => {
+            handleShow(item);
+          }}
+        >
           <div key={index} className="myDiv">
             <img src={item.avatar} alt="he" className="myImg" />
             <div className="myText">{item.email}</div>
